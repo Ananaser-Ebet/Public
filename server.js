258a3ef13243faeabfdb5715d9f1bfcd0cb724c9
Ananaser <ananaser.off@gmail.com>
2026-05-11 13:29:42 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const REAL_PASSWORD = process.env.PASSWORD || 'Jt8mBrY2eD6vNxP4qKwSf';
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('devast proxy by ananaser');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (clientWS) => {
+    console.log('client connected');
+
+    const proxyWS = new WebSocket('wss://devast.us/?' + REAL_PASSWORD);
+
+    proxyWS.on('message', (data) => {
+        clientWS.send(data);
+    });
+
+    clientWS.on('message', (data) => {
+        proxyWS.send(data);
+    });
+
+    proxyWS.on('close', () => clientWS.close());
+    clientWS.on('close', () => proxyWS.close());
+});
+
+server.listen(PORT, () => {
+    console.log('proxy running on port ' + PORT);
+});
